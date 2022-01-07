736c098eb555112f1d7af6079b3d0461e5e92657
Mike Pepper <mdcpepper@gmail.com>
2022-01-07 06:16:04 +0000

    Add new joke action

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,3 +1,29 @@
+import type {ActionFunction} from "remix";
+import {redirect} from "remix";
+import {db} from "~/utils/db.server";
+
+export const action: ActionFunction = async ({
+  request
+}) => {
+  const form = await request.formData();
+  const name = form.get("name");
+  const content = form.get("content");
+  // we do this type check to be extra sure and to make TypScript happy
+  // we'll explore validation next!
+  if (
+    typeof name !== "string" ||
+    typeof content !== "string"
+  ) {
+    throw new Error(`Form not submitted correctly`);
+  }
+
+  const fields = {name, content};
+
+  const joke = await db.joke.create({ data: fields });
+
+  return redirect(`/jokes/${joke.id}`);
+};
+
 export default function NewJokeRoute() {
   return (
     <div>
@@ -5,12 +31,12 @@ export default function NewJokeRoute() {
       <form method="post">
         <div>
           <label>
-            Name: <input type="text" name="name" />
+            Name: <input type="text" name="name"/>
           </label>
         </div>
         <div>
           <label>
-            Content: <textarea name="content" />
+            Content: <textarea name="content"/>
           </label>
         </div>
         <div>
