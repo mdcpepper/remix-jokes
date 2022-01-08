e95c3eddcfcccaa0bac96bd05400a749e8ecbb49
Mike Pepper <mdcpepper@gmail.com>
2022-01-08 10:16:17 +0000

    Add login functionality

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,6 +1,7 @@
 import type {ActionFunction} from "remix";
 import {useActionData, redirect, json} from "remix";
-import {db} from "~/utils/db.server";
+import { db } from "~/utils/db.server";
+import { requireUserId } from "~/utils/session.server";
 
 function validateJokeName(name: string) {
   if (name.length < 2) {
@@ -31,6 +32,7 @@ const badRequest = (data: ActionData) => json(data, { status: 400});
 export const action: ActionFunction = async ({
   request
 }) => {
+  const userId = await requireUserId(request);
   const form = await request.formData();
   const name = form.get("name");
   const content = form.get("content");
@@ -54,7 +56,9 @@ export const action: ActionFunction = async ({
     return badRequest({ fieldErrors, fields});
   }
 
-  const joke = await db.joke.create({ data: fields });
+  const joke = await db.joke.create({
+    data: { ...fields, jokesterId: userId }
+  });
 
   return redirect(`/jokes/${joke.id}`);
 };
@@ -85,6 +89,7 @@ export default function NewJokeRoute() {
                   ? "name-error"
                   : undefined
               }
+              required
             />
           </label>
           {actionData?.fieldErrors?.name ? (
@@ -112,6 +117,7 @@ export default function NewJokeRoute() {
                   ? "content-error"
                   : undefined
               }
+              required
             />
           </label>
           {actionData?.fieldErrors?.content ? (
